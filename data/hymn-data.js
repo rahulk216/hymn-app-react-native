bfd8605b745a0692543e9926ad73de948f5cc370
Rahul Kakkadan <rkakkadan@enphaseenergy.com>
2023-11-11 00:30:56 +0530

    Added bottom navbar and started with FlatList for homepage

---
+++ data/hymn-data.js
@@ -0,0 +1,394 @@
+export const hymnData = [
+    {
+        id: 1,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 2,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 3,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 4,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 5,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 6,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 1,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 2,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 3,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 4,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 5,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 6,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 1,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 2,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 3,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 4,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 5,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    },
+    {
+        id: 6,
+        hymnNumber: 317,
+        title:'Karthana sthothrava madirella',
+        lyrics: "Karthana sthothrava madirella",
+        mtNumber: [12],
+        songCategory: 1,
+        language: 'kan',
+        letterCategory: 1,
+        author:'Moegling',
+        tune: [
+            {
+                number: 1,
+                tune: 'stringUrl'
+            },
+            {
+                number: 2,
+                tune: 'stringUrl'
+            }
+        ]
+    }
+]
+
+const songCategory = [
+    {
+        id: 1,
+        category: 'Deva Sthothra',
+    }
+]
+
+const letterCategory = [
+    {
+        id: 1,
+        letterCategory: 'a'
+    }
+]
